7509af6c929869a4f05038d590e2db68fff2c286
Aditya Kumar Singh <210110168@mitmoradabad.edu.in>
2024-09-09 08:23:47 +0530

    made color changes

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+// components/NavBar.js
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav class="navbar">
+    <div class="navbar-brand">BrandName</div>
+     <ul class="nav-links">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Features</a></li>
+    <li><a href="#">Testimonials</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+</nav>
+
+  );
+};
+
+export default NavBar;
